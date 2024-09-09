011012d31645b95d9c31e98897ab9b6568cd6cf8
Mika Auranen <auranenmika@gmail.com>
2024-09-09 17:08:00 +0300

    Lisätty if -else - ehtolause

--- arvauspeli/src/arvauspeli.java
+++ arvauspeli/src/arvauspeli.java
@@ -2,8 +2,18 @@ import java.util.Scanner;
 
 public class arvauspeli {
     public static void main(String[] args) throws Exception {
-    Scanner scanner = new Scanner(System.in);
+    Scanner in = new Scanner(System.in); 
+
     String nimi = ("Mika");
+    String input;
+
     System.out.println("Arvaa nimeni!");
+    input = in.nextLine();
+    if (input.equals(nimi)) {
+        System.out.println("Oikein!");
     }
+    else {
+        System.out.println("Väärin!");
+    }
+}
 }
