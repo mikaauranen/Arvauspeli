c7bfa19438ff3477641c629b1695a5e2689b232a
Mika Auranen <auranenmika@gmail.com>
2024-09-09 16:36:40 +0300

    Nimi ja kysymys lisätty

---
+++ arvauspeli/src/arvauspeli.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; 
+
+public class arvauspeli {
+    public static void main(String[] args) throws Exception {
+    Scanner scanner = new Scanner(System.in);
+    String nimi = ("Mika");
+    System.out.println("Arvaa nimeni!");
+    }
+}
